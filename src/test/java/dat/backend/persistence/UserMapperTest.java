12234d8652cb285b0dfa6f67e614ce2ab677429a
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-03-20 14:05:02 +0100

    Update UserMapperTest.java

--- src/test/java/dat/backend/persistence/UserMapperTest.java
+++ src/test/java/dat/backend/persistence/UserMapperTest.java
@@ -19,7 +19,7 @@ class UserMapperTest
     // TODO: Change mysql login credentials if needed below
 
     private final static String USER = "root";
-    private final static String PASSWORD = "oo123";
+    private final static String PASSWORD = "Frederik1988";
     private final static String URL = "jdbc:mysql://localhost:3306/oskarsmuffins_test?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
 
     private static ConnectionPool connectionPool;
@@ -34,10 +34,10 @@ class UserMapperTest
             try (Statement stmt = testConnection.createStatement())
             {
                 // Create test database - if not exist
-                stmt.execute("CREATE DATABASE  IF NOT EXISTS oskarscupcakes_test;");
+                stmt.execute("CREATE DATABASE  IF NOT EXISTS oskarsmuffins_test;");
 
                 // TODO: Create user table. Add your own tables here
-                stmt.execute("CREATE TABLE IF NOT EXISTS olskercupcakes_test.user LIKE olskercupcakes.user;");
+                stmt.execute("CREATE TABLE IF NOT EXISTS olskercupcakes_test.user LIKE oskarsmuffins.user;");
             }
         }
         catch (SQLException throwables)
